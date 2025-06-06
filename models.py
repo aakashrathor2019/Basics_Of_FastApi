9e4141f5ac630bb0161d45888f11c43bd26d28cf
Aakash Rathor <aakashrathor2019@gmail.com>
2025-06-06 12:57:25 +0530

    create models

---
+++ models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class NewUser(Base):
+    __tablename__ = "newuser"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    age = Column(Integer)
+    email = Column(String, unique=True)
+    mo = Column(String)
